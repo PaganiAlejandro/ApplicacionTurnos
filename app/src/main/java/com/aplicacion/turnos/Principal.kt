5036c2eee93f5f1bf9341e0e2bf4719e9fab5d51
Alejandro Pagani <alejandro.pagani@rappi.com>
2020-05-05 11:59:50 -0300

    Initial commit

---
+++ app/src/main/java/com/aplicacion/turnos/Principal.kt
@@ -0,0 +1,12 @@
+package com.aplicacion.turnos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Principal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
